de485264d891cc5e6d32bae24dfd30a8dc745151
Creta5164 <creta5164@gmail.com>
2017-07-24 21:13:32 +0900

    Initial commit

---
+++ main.js
@@ -0,0 +1,31 @@
+﻿var loginState;
+
+function init()
+{
+    loginState = false;
+    Trello.authorize({
+        type: 'popup',
+        name: 'Trello 스케줄러',
+        scope: {
+            read: 'true',
+            write: 'true'
+        },
+        expiration: 'never',
+        success: Trello_LoginSuccess,
+        error: Trello_LoginFail
+    });
+}
+
+//트렐로 로그인 성공
+function Trello_LoginSuccess()
+{
+    document.body.style.background = "#fff";
+    loginState = true;
+}
+
+//트렐로 로그인 실패
+function Trello_LoginFail() {
+    document.body.style.background = "#f00";
+}
+
+init();
