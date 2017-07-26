e7804aba39d8136cbda3d9089e4e8c36829b811f
Creta5164 <creta5164@gmail.com>
2017-07-26 15:52:56 +0900

    loading content with step by step style

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 ﻿let loginState;
+let programLoaded;
 const schedulerBoardName = "TrelloScheduler";
 let schedulerBoardData;
 
 function init() {
-
+    loginState = false;
+    programLoaded = false;
 }
 
 function loginTrello() {
@@ -25,13 +27,14 @@ function onAuthorize() {
     loginState = Trello.authorized();
     document.getElementById("logout").style.display = loginState ? "list-item" : "none";
 
-    if (loginState) loadProgram();
+    if (loginState && !programLoaded) loadProgram(0);
 }
 
 //트렐로 로그아웃
 function logoutTrello() {
     Trello.deauthorize();
     onAuthorize();
+    programLoaded = false;
     window.location.reload();
 }
 
@@ -39,7 +42,7 @@ function logoutTrello() {
 function Trello_LoginSuccess() {
     loginState = true;
     document.getElementById("logout").style.display = "list-item";
-    loadProgram();
+    loadProgram(0);
 }
 
 //트렐로 로그인 실패
@@ -48,34 +51,47 @@ function Trello_LoginFail() {
 }
 
 //Trello 스케줄러를 시작합니다.
-function loadProgram() {
-    Trello.get("/members/me/boards/",
-        CheckSchedulerBoardExists,
-        LoadFailed
-    );
+function loadProgram(steps) {
+    if (programLoaded) return;
+    switch (steps) {
+        default:
+            logoutTrello();
+            return;
+
+        case 0:
+            Trello.get("/members/me/boards/",
+                CheckSchedulerBoardExists,
+                LoadFailed
+            );
+            return;
+        case 1:
+
+            return;
+    }
+
+    //finish a loading content
+    programLoaded = true;
 }
 
 function CheckSchedulerBoardExists(list) {
-    console.log("보드 색인 중...");
-
-    for (let i = 0, len = list.length; i < len; i++)
-        if (list[i].name === schedulerBoardName) {
-            console.log("보드를 발견함.");
+    if (programLoaded) return;
+    for (var i = 0, len = list.length; i < len; i++)
+        if (list[i].name == schedulerBoardName) {
             schedulerBoardData = list[i];
+            loadProgram(1);
             return;
         }
 
-    console.log("보드를 생성함.");
     Trello.rest('POST', 'boards', {
         name: schedulerBoardName,
         desc: "Trello 스케줄러가 사용하는 보드입니다, 특별한 일이 아니라면 삭제하거나 편집하지 마세요!"
     },
-    function (result) { loadProgram(); },
+    function (result) { loadProgram(0); },
     LoadFailed);
 }
 
-function LoadFailed()
-{
+function LoadFailed() {
+    programLoaded = false;
     alert("통신하는 도중, 오류가 발생했습니다.");
     logoutTrello();
 }
