7e21a4e30a6fab908ee58dbc5a5f12a48eaefa2f
Creta <creta5164@gmail.com>
2017-08-16 17:05:34 +0900

    test

--- main.js
+++ main.js
@@ -35,7 +35,7 @@ function init() {
     updateLoginElements();
 
     if (screenfull.enabled)
-        screenfull.request();
+        setTimeout(function () { screenfull.request(); }, 1000);
 }
 
 //로그인 상태를 확인하고, 표시할 것과 표시되지 말아야 할 것을 업데이트합니다.
