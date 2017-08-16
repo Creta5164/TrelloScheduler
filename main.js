3b672a20748705bf1f5a90af9e502b64fb6b9411
Creta <creta5164@gmail.com>
2017-08-16 16:38:28 +0900

    test

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ function init() {
 
     updateLoginElements();
 
-    if (isMobile) document.documentElement.requestFullscreen();
+    if (isMobile) document.documentElement.webkitRequestFullscreen();
 }
 
 //로그인 상태를 확인하고, 표시할 것과 표시되지 말아야 할 것을 업데이트합니다.
