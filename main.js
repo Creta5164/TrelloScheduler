1896dbf2bb0c67032d2d3c6226766e2c0bf56ea8
Creta <creta5164@gmail.com>
2017-08-16 16:54:55 +0900

    test with screenfull.js

--- main.js
+++ main.js
@@ -34,7 +34,8 @@ function init() {
 
     updateLoginElements();
 
-    if (isMobile) document.documentElement.webkitRequestFullscreen();
+    if (isMobile && screenfull.enabled)
+        screenfull.request();
 }
 
 //로그인 상태를 확인하고, 표시할 것과 표시되지 말아야 할 것을 업데이트합니다.
